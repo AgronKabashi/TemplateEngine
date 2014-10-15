a3bcf1de4af5425eb13e6d82e26b9c3fe19de5e4
AgronKabashi <agron.kabashi@tretton37.com>
2014-10-15 18:25:17 +0200

    Added empty Text editor controller

---
+++ src/TemplateEditor/Controller/ControlPluginEditor/Basic/Text.js
@@ -0,0 +1,11 @@
+﻿define(
+	[
+		"../../../App.js"
+	],
+	function (templateEditorModule)
+	{
+		var controllerId = "Cerberus.Tool.TemplateEditor.Controller.ControlPluginEditor.Basic.Text";
+		templateEditorModule.controller(controllerId, function () { });
+
+		return controllerId;
+	});
